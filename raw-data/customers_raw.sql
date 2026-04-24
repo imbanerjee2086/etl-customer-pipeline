5ab942a7905ae4a97456d578ee0f2113466731cf
Raj Banerjee <34864796+imbanerjee2086@users.noreply.github.com>
2026-04-24 18:42:56 +0530

    Update customers_raw.sql

--- raw-data/customers_raw.sql
+++ raw-data/customers_raw.sql
@@ -1,1 +1,12 @@
+CREATE TABLE customers_raw (
+    id INT,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    country VARCHAR(50)
+);
 
+INSERT INTO customers_raw VALUES
+(1, 'Raj', 'raj@email.com', 'India'),
+(2, 'Amit', NULL, 'India'),
+(3, 'Sneha', 'sneha@email.com', NULL),
+(4, 'John', 'john@email.com', 'USA');
